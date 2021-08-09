112046a5009b9cdd9e2021da0aafaded1ead659c
Matheus Ribeiro Alves <57848780+MathHardW@users.noreply.github.com>
2021-08-09 16:55:14 -0300

    09/08 - 16:54 (Matheus Alves)

--- src/Components/EcommercePage/PaymentDescription/style.js
+++ src/Components/EcommercePage/PaymentDescription/style.js
@@ -1,21 +1,5 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: flex-start;
-
-    width: 58%;
-    padding-left: 50px;
-    padding-top: 30px;
-    padding-right: 100px;
-
-    height: 1000px;
-
-    background-color: #F9FBFF;
-`;
-
 export const Title = styled.h2`
     color: var(--blue);
 
---
+++ src/Pages/EcommercePage/Payment/Sections/CenterSection/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+
+    width: 58%;
+    padding-left: 50px;
+    padding-top: 30px;
+    padding-right: 100px;
+
+    height: 1000px;
+
+    background-color: #F9FBFF;
+`;
